3460032ae0b6c2434a07c94dbe1aaf6977ca38d2
Eff <peter.felber@turbine.ai>
2020-03-26 23:32:14 +0100

    Updates

---
+++ precache-manifest.2730018aa122197557ab978beea457af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e1da0be735611e7e5c452becea7411",
+    "url": "/smart-roux-timer/index.html"
+  },
+  {
+    "revision": "faafd4c440a580e762a5",
+    "url": "/smart-roux-timer/static/css/main.b71a72dd.chunk.css"
+  },
+  {
+    "revision": "951d82a9b6ba86928992",
+    "url": "/smart-roux-timer/static/js/2.30a0bd48.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/smart-roux-timer/static/js/2.30a0bd48.chunk.js.LICENSE"
+  },
+  {
+    "revision": "faafd4c440a580e762a5",
+    "url": "/smart-roux-timer/static/js/main.27e5f9f2.chunk.js"
+  },
+  {
+    "revision": "600f13b689e521728963",
+    "url": "/smart-roux-timer/static/js/runtime-main.b217cc07.js"
+  }
+]);
