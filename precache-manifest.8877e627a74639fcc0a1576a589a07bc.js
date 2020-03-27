6a27817a64bd4bd34fe5aea2a91ef292a07eaa48
Eff <peter.felber@turbine.ai>
2020-03-27 01:04:09 +0100

    Updates

--- precache-manifest.8877e627a74639fcc0a1576a589a07bc.js
+++ precache-manifest.8877e627a74639fcc0a1576a589a07bc.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dd8e482b41b4dc79b858f9d7e431ff68",
+    "revision": "214ff0aef3bc6e0ce8118a14fee02c7f",
     "url": "/smart-roux-timer/index.html"
   },
   {
-    "revision": "41067966087b5af211b9",
+    "revision": "a1811197d3c910d6c211",
     "url": "/smart-roux-timer/static/css/main.b71a72dd.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/smart-roux-timer/static/js/2.5a9b6907.chunk.js.LICENSE"
   },
   {
-    "revision": "41067966087b5af211b9",
-    "url": "/smart-roux-timer/static/js/main.e5967d0f.chunk.js"
+    "revision": "a1811197d3c910d6c211",
+    "url": "/smart-roux-timer/static/js/main.a4d5f86e.chunk.js"
   },
   {
     "revision": "600f13b689e521728963",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/smart-roux-timer/precache-manifest.f2883cb9ff6d72279e8e2efcd015e14c.js"
+  "/smart-roux-timer/precache-manifest.8877e627a74639fcc0a1576a589a07bc.js"
 );
 
 self.addEventListener('message', (event) => {
