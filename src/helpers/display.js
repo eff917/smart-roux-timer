aef178b3f789789a922e32e28d2ae1cd0a096c6c
Eff <peter.felber@turbine.ai>
2020-03-27 23:03:52 +0100

    random colors for each facelet

--- src/helpers/display.js
+++ src/helpers/display.js
@@ -9,7 +9,8 @@ import {
     FaceColors,
     Color,
     Vector3,
-    Colors
+    Colors,
+    VertexColors
   } from "three";
   
   
@@ -30,37 +31,7 @@ var renderer = new WebGLRenderer();
 renderer.setSize( 600, 600 );
 document.body.appendChild( renderer.domElement );
 
-
-//var geometry = new BoxGeometry();
-// geometry.faces[0].color.setHex(cubeColor[5]);
-// geometry.faces[1].color.setHex(cubeColor[5]);
-
-// geometry.faces[2].color.setHex(cubeColor[3]);
-// geometry.faces[3].color.setHex(cubeColor[3]);
-
-// geometry.faces[4].color.setHex(cubeColor[1]);
-// geometry.faces[5].color.setHex(cubeColor[1]);
-
-// geometry.faces[6].color.setHex(cubeColor[6]);
-// geometry.faces[7].color.setHex(cubeColor[6]);
-
-// geometry.faces[8].color.setHex(cubeColor[4]);
-// geometry.faces[9].color.setHex(cubeColor[4]);
-
-// geometry.faces[10].color.setHex(cubeColor[2]);
-// geometry.faces[11].color.setHex(cubeColor[2]);
-
-// for ( var i = 0; i < geometry.faces.length; i ++ ) {
-//     geometry.faces[ i ].color.setHex( cubeColor[~~(i/2)+1] );
-// };
-// var material = new MeshBasicMaterial(
-//     {
-//         color: 0xffffff,
-//         vertexColors: FaceColors
-//        }
-// );
-//geometry.colorsNeedUpdate = true
-var geometry = new BoxBufferGeometry( 3, 3, 3, 3, 3, 3);
+// define colorGroup to choose color from
 var material = [
     new MeshBasicMaterial( {color: cubeColor[5]} ),
     new MeshBasicMaterial( {color: cubeColor[3]} ),
@@ -68,19 +39,16 @@ var material = [
     new MeshBasicMaterial( {color: cubeColor[6]} ),
     new MeshBasicMaterial( {color: cubeColor[4]} ),
     new MeshBasicMaterial( {color: cubeColor[2]} ),
-]
-// geometry.addGroup(0, 2, 0)
-// geometry.addGroup(2, 2, 0)
-// geometry.addGroup(4, 2, 0)
-// geometry.addGroup(6, 2, 0)
-// geometry.addGroup(8, 2, 0)
-// geometry.addGroup(10, 2, 0)
-// geometry.addGroup(12, 2, 0)
-// geometry.addGroup(14, 2, 0)
-// geometry.addGroup(16, 2, 0)
+];
+var geometry = new BoxBufferGeometry( 3, 3, 3, 3, 3, 3);
 
-material.vertexColors = false;
-material.wireframe = true;
+// delete default face groups, and define groups for each cubie
+geometry.clearGroups();
+for(let i=0; i<geometry.index.count; i+=6) {
+    geometry.addGroup(i, 6, Math.floor(Math.random() * 6)); // the last number is the index of the color from material list
+}
+
+// combine colors and geometry
 var cube = new Mesh( geometry, material);
 console.log(cube)
 scene.add( cube );
